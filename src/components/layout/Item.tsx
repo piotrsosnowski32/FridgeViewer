16a3eed48003b44b3c8b624dc8f4caa4ac81e24c
piotrsosnowski32 <piotr.sosnowski31@gmail.com>
2025-10-08 21:50:55 +0200

    main frame design

--- src/components/layout/Item.tsx
+++ src/components/layout/Item.tsx
@@ -1,23 +1,53 @@
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
+import { format } from 'date-fns';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 interface ItemProps {
     id: number;
     data: {
         name : string;
-        buy_date : string;
-        toeat_date : string | null;
+        buy_date : string | null;
+        toeat_date : string;
         category : string;
     }
 };
 
+
 export const Item = ( { id, data } : ItemProps ) => {
-    console.log()
+    const [innerStyle, setInnerStyle] = useState('#b0eeadff');
+    const [borderStyle, setborderStyle] = useState('solid #1dee12ff');
+
+    const today = new Date().valueOf(); 
+    const toEatDate = new Date(data.toeat_date).valueOf();
+    const difference = Math.ceil((toEatDate - today) / (1000 * 3600 * 24));
+
+    useEffect(() => {
+        if (difference < 2 && difference > 0) {
+                setInnerStyle('#eec58fff');
+                setborderStyle('solid #ff950aff');
+        } else if (difference < 0) {
+                setInnerStyle('#f7b8b8ff');
+                setborderStyle('solid #f70000ff');
+        }
+    }, [difference])
+
     return (
-        <ListItem style={ { width: '90%' } }>
-            <ListItemButton sx={ { backgroundColor: '#FCB53B' } }>NAZWA: {data.name}  WAŻNE DO: {data.toeat_date} ZAKUPIONO: {data.buy_date}</ListItemButton>
+        <ListItem style={ { width: '90%', borderRadius: '10px' } }>
+            <ListItemButton sx={ { backgroundColor: innerStyle, border: borderStyle , borderRadius: '10px', display:'flex', justifyContent:'space-between' } }>
+                <div style = { { display: 'flex', flexDirection: 'column' } }>
+                    <span>{data.name}</span>
+                    <span>zakupiono: {data.buy_date}</span>
+                </div>
+                <div style = { { display: 'flex', flexDirection: 'column' } }> 
+                    {data.toeat_date}
+                    <span>Do końca: {difference}</span>
+                </div>              
+             </ListItemButton>
         </ListItem>
     );
 };
 
-// data={data}
+
+// data={data} ZAKUPIONO: {data.buy_date} <p style={ { margin:'0' } }>{CalculateDays(data.toeat_date)}{CalculateDays(data.toeat_date)}</p>
--- src/components/ui/mainFrame.tsx
+++ src/components/ui/mainFrame.tsx
@@ -5,18 +5,18 @@ import Container from '@mui/material/Container';
 
 export default function MainFrame() {
     const items = [
-        {'id':0, 'name': 'marchewka','buy_date':'2025-09-27', 'toeat_date': null, 'category': 'warzywa'},
-        {'id':1, 'name': 'kurczak','buy_date':'2025-09-26', 'toeat_date': '2025-09-30', 'category': 'mieso'},
+        {'id':0, 'name': 'marchewka','buy_date':null, 'toeat_date': '2025-09-27', 'category': 'warzywa'},
+        {'id':1, 'name': 'kurczak','buy_date':'2025-09-26', 'toeat_date': '2025-10-09', 'category': 'mieso'},
         {'id':2, 'name': 'skyr','buy_date':'2025-09-24', 'toeat_date': '2025-10-10', 'category': 'nabial'}
     ];
     
     return (
-        <Container style={ { display:"flex", flexDirection:'column', backgroundColor: '#FFE797', width: '100%', margin:0, maxWidth:'3000px', minHeight:'3000px', padding:0 } }>
+        <Container style={ { display:"flex", flexDirection:'column', backgroundColor: '#e9ddb6ff', width: '100%', margin:0, maxWidth:'3000px', minHeight:'3000px', padding:0 } }>
 
             <header style={ { height:'5%', backgroundColor:"#272727", width: '100%', color:'white' } }>FRIDGE VIEWER</header>
 
             <Container sx={ { height:"90%", padding:0, width: '50%' } }>
-                <List style={ { height:"100%", display: "flex", flexDirection: "column", alignItems: 'center' } }>
+                <List style={ { height:"100%", display: "flex", flexDirection: "column", alignItems: 'center', padding:0 } }>
                     {items.map(( { id, name, buy_date, toeat_date, category } ) => (
                         <Item key={id} id={id} data={ { name, buy_date, toeat_date, category } }/>
                     ))}
