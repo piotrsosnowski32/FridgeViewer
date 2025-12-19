7246697e45efaced062fb8dcc239b31d7f796948
piotr <piotr.sosnowski31@gmail.com>
2025-12-19 20:09:31 +0100

    add button

--- src/components/layout/AddButton.tsx
+++ src/components/layout/AddButton.tsx
@@ -1,30 +1,84 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { Field } from '@base-ui/react/field';
 
-const AddButton = () => {
+
+const AddButton = ( {onActivate} ) => {
     const fabStyle:{} = {
-        position: "fixed",
-        bottom: "60px",
+        position: "relative",
         right: "20px",
-        width: "60px",
-        height: "60px",
+        height: "90%",
         borderRadius: "50%",
         backgroundColor: "#272727",
         color: "#fff",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        fontSize: "32px",
+        fontSize: "6vh",
         boxShadow: "0 4px 6px rgba(0,0,0,0.3)",
         cursor: "pointer",
         border: "none",
+        aspectRatio: 1/1
   };
 
+    const [showField, setShowField] = useState(false);
+ 
+    const handleClick = () => {
+      if (!showField) {
+        setShowField(true);       
+      } else {
+        setShowField(false);
+      };
+      onActivate();
+    };
+
   return (
-    <button style={fabStyle}>
-      +
-    </button>
+    <>
+      <button style={fabStyle} onClick={handleClick}>
+        +
+      </button>
+      {showField && (
+          <Field.Root className="flex w-full max-w-64 flex-col items-start gap-1" style={{ position:"fixed", right:"50%", bottom:"50%", display:"flex", flexDirection:"column" }}>
+            <Field.Label className="text-sm font-medium text-gray-900">Name</Field.Label>
+            <Field.Control
+              required
+              placeholder="Required"
+              className="h-10 w-full rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+            />
+            <Field.Error className="text-sm text-red-800" match="valueMissing">
+              Please enter your name
+            </Field.Error>
+
+            <Field.Label className="text-sm font-medium text-gray-900">Name</Field.Label>
+            <Field.Control
+              required
+              placeholder="Required"
+              className="h-10 w-full rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+            />
+
+            <Field.Label className="text-sm font-medium text-gray-900">Name</Field.Label>
+            <Field.Control
+              required
+              placeholder="Required"
+              className="h-10 w-full rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+            />
+
+            <Field.Label className="text-sm font-medium text-gray-900">Name</Field.Label>
+            <Field.Control
+              required
+              placeholder="Required"
+              className="h-10 w-full rounded-md border border-gray-200 pl-3.5 text-base text-gray-900 focus:outline focus:outline-2 focus:-outline-offset-1 focus:outline-blue-800"
+            />
+
+          </Field.Root>
+      )}
+        
+    </>
+
   );
 
 };
 
 export default AddButton;
+
+// anchorEl={anchorEl} 
--- src/components/ui/mainFrame.tsx
+++ src/components/ui/mainFrame.tsx
@@ -9,33 +9,45 @@ import AddButton from "../layout/AddButton.tsx";
 
 export default function MainFrame() {
     const [items, setItems] = useState<ItemProps[]>([]);
+      const [blurred, setBlurred] = useState(false);
 
     useEffect(() => {
         product.getProducts()
             .then(data => setItems(data));
     }, []);
 
-    return (
-        <Container style={ { display:"flex", flexDirection:'column', backgroundColor: '#e9ddb6ff', margin:0, minHeight:'3000px', padding:0, maxWidth: '100%' } }>
+    const handleClick = () => {
 
+    };
+
+    return (
+        <Container style={ { display:"flex", flexDirection:'column', backgroundColor: '#e9ddb6ff', margin:0, padding:0, maxWidth: '100%', height: "100vh" } }>
             <header style={ { height:'5%', backgroundColor:"#272727", color:'white' } }>FRIDGE VIEWER</header>
 
-            <Container sx={ { height:"90%", padding:0 } }>
-                <List style={ { height:"100%", display: "flex", flexDirection: "column", alignItems: 'center', padding:0 } }>
-                    {!items || items?.length === 0 ? <div>Loading...</div> :
-                        items.map(( { id, name, purchase_date, expiry_date, category } ) => ( 
-                            <Item 
-                                key={id} 
-                                id={id} 
-                                name={ name } 
-                                purchase_date={purchase_date} 
-                                expiry_date={expiry_date} 
-                                category={category} /> ))
-                    }
-                </List>
-            </Container>
-            <AddButton />        
-            <footer style={ { height:'5%', width: '100%', position:'fixed',  left: 0, bottom: 0, backgroundColor:"#272727", color:'white'  } }>&reg; TEST</footer>
+            <div style={{height:"80%",filter: blurred ? "blur(4px)" : "none"}}> 
+
+                <Container sx={ {  padding:0 } }>
+                    <List style={ { height:"100%", display: "flex", flexDirection: "column", alignItems: 'center', padding:0 } }>
+                        {!items || items?.length === 0 ? <div>Loading...</div> :
+                            items.map(( { id, name, purchase_date, expiry_date, category } ) => ( 
+                                <Item 
+                                    key={id} 
+                                    id={id} 
+                                    name={ name } 
+                                    purchase_date={purchase_date} 
+                                    expiry_date={expiry_date} 
+                                    category={category} /> ))
+                        }
+                    </List>
+                </Container>
+            
+            </div>
+
+            <div style={{ display:"flex", justifyContent:"flex-end", height:"10%" }}>
+                <AddButton onActivate={() => setBlurred(prev => !prev)} />  
+            </div>    
+
+            <footer style={ { height:'5%', width: '100%', position:'absolute',  left: 0, bottom: 0, backgroundColor:"#272727", color:'white'  } }>&reg; TEST</footer>
 
         </Container>
     )
